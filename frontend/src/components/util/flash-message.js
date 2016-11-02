cbc73eb69acd7f2644f018d77c4bb6eff9a5249b
Anthonius Munthi <me@itstoni.com>
2016-11-02 23:57:59 +0800

    configured build path

---
+++ frontend/src/components/util/flash-message.js
@@ -0,0 +1,49 @@
+import shortid from 'shortid';
+import findIndex from 'lodash/findIndex';
+
+const ADD       = 'flash.add';
+const REMOVE    = 'flash.remove';
+
+export function addFlashMessage(type,message){
+    return {
+        type: ADD,
+        payload: {
+            type: type,
+            text: message
+        }
+    }
+}
+
+export function removeFlashMessage(id){
+    return {
+        type: REMOVE,
+        payload: {
+            id: id
+        }
+    }
+}
+
+export default (state = [], action = {}) => {
+    switch(action.type) {
+        case ADD:
+            return [
+                ...state,
+                {
+                    id: shortid.generate(),
+                    type: action.payload.type,
+                    text: action.payload.text
+                }
+            ];
+        case REMOVE:
+            const index = findIndex(state, { id: action.payload.id });
+            if (index >= 0) {
+                return [
+                    ...state.slice(0, index),
+                    ...state.slice(index + 1)
+                ];
+            }
+            return state;
+
+        default: return state;
+    }
+}
---
+++ frontend/src/views/common/flash-message.js
@@ -0,0 +1,37 @@
+import React, {
+    Component,
+    PropTypes,
+} from 'react';
+
+import classNames from 'classnames';
+
+class FlashMessage extends Component {
+    constructor(props) {
+        super(props);
+        this.onClick = this.onClick.bind(this);
+    }
+
+    onClick() {
+        this.props.removeFlashMessage(this.props.message.id);
+    }
+
+    render() {
+        const { id, type, text } = this.props.message;
+        return (
+            <div className={classNames('alert', {
+                'alert-success': type === 'success',
+                'alert-danger': type === 'error'
+            })}>
+                <button id={id} onClick={this.onClick} className="close"><span>&times;</span></button>
+                {text}
+            </div>
+        );
+    }
+}
+
+FlashMessage.propTypes = {
+    message: PropTypes.object.isRequired,
+    removeFlashMessage: PropTypes.func.isRequired
+};
+
+export default FlashMessage;
