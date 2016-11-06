25bbb26040b3301a30c7731e975f20b318eda430
Anthonius Munthi <me@itstoni.com>
2016-11-06 21:12:54 +0800

    added <TextFieldGroup/> specs

--- frontend/src/views/common/TextFieldGroup.js
+++ frontend/src/views/common/TextFieldGroup.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import classNames from 'classnames';
 
 const TextFieldGroup = ({input,label,type,meta:{touched,error,warning}}) => {
     return (
-        <div className="form-group">
+        <div className={classNames('form-group', {'has-error': touched && error},{'has-warning': touched && warning})}>
             <label htmlFor={input.name} className="control-label">
+                {touched && warning && <i className="fa fa-bell-o"/>}
+                {touched && error && <i className="fa fa-times-circle-o"/>}
                 {label}
             </label>
             <input id={input.name} name={input.name} type={type} className="form-control" {...input}/>
+            {touched && error && <div className="help-block">{error}</div> }
+            {touched && warning && <div className="help-block">{warning}</div> }
         </div>
     );
 };
---
+++ frontend/src/views/common/TextFieldGroup.spec.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import TextFieldGroup from './TextFieldGroup';
+
+function getComponent(props){
+    return mount(<TextFieldGroup {...props}/>);
+}
+
+describe('<TextFieldGroup/>', () => {
+
+    let props;
+
+    beforeEach(() => {
+        props = {
+            input: {
+                name: 'inputName',
+            },
+            label: 'input label',
+            meta: {
+                touched: false,
+                error: false,
+                warning: false
+            }
+        }
+    });
+
+    it('should rendered properly', () => {
+        props.meta.touched = true;
+        const wrapper = getComponent(props);
+        const html = wrapper.html();
+
+        expect(html).toContain('input label');
+        expect(html).toContain('id="inputName"');
+        expect(html).toContain('name="inputName"');
+        expect(html).toContain('for="inputName"');
+        expect(html).toContain('type="text"');
+
+        expect(html).not.toContain('has-error');
+        expect(html).not.toContain('has-warning');
+    });
+
+    it('should render password type', () => {
+        props.type = 'password';
+        const wrapper = getComponent(props);
+        const html = wrapper.html();
+        expect(html).toContain('type="password"');
+    });
+
+    it('should render warning message', () => {
+
+        props.meta.touched = true;
+
+        props.meta.warning = 'warning message';
+
+        const wrapper = getComponent(props);
+        const html = wrapper.html();
+        expect(wrapper.find('.has-warning').length).toBe(1);
+        expect(html).toContain('warning message');
+    });
+
+    it('should render error message', () => {
+
+        props.meta.touched = true;
+        props.meta.error = 'error message';
+
+        const wrapper = getComponent(props);
+        const html = wrapper.html();
+        expect(wrapper.find('.has-error').length).toBe(1);
+        expect(wrapper.find('.fa-times-circle-o').length).toBe(1);
+        expect(html).toContain('error message');
+    });
+
+});
