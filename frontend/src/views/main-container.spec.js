64602e66a9bb96f195d13057330c6d7bbe34dd7e
Anthonius Munthi <me@itstoni.com>
2016-11-09 11:55:45 +0800

    added more spec for <MainContainer/> unrecovered lines

--- frontend/src/views/main-container.spec.js
+++ frontend/src/views/main-container.spec.js
@@ -28,6 +28,23 @@ describe('MainContainer View', () => {
         expect(body.classList.contains('sidebar-mini')).toBeTruthy();
     });
 
+    it('should not add sidebar-mini css class when it\'s already exists in body', () => {
+        const body = document.body;
+        body.className = 'sidebar-mini skin-blue some-css';
+
+        const wrapper = shallow(
+            <MainContainer
+                title="Test"
+                subtitle="Subtitle"
+                removeFlashMessage={removeFlashMessage}
+                store={store}
+            />
+        ).shallow();
+
+        expect(body.classList.contains('sidebar-mini')).toBeTruthy();
+        expect(body.classList.contains('some-css')).toBeTruthy();
+    });
+
     it('should render login layout', () => {
         const wrapper = shallow(
             <MainContainer
